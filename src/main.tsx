9ecd5f07144e25dc1bf2dcd23d50df1759861c5f
KenanSeckanovic <kenanseckanovic23@gmail.com>
2026-01-08 20:52:43 +0100

    docs(app): add TSDoc to application entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,9 @@
+/**
+ * Einstiegspunkt der React-Anwendung.
+ *
+ * Initialisiert das React-Root-Element und rendert die App-Komponente.
+ */
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
